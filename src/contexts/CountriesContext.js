748fe079fa78c3166cb3c87012ce0b9d508eeff8
Geoffrey Siele <sielekrisp@gmail.com>
2020-01-26 09:59:45 +0300

    Remove dead code from src/contexts/CountriesContext.js

--- src/contexts/CountriesContext.js
+++ src/contexts/CountriesContext.js
@@ -45,39 +45,3 @@ const SetContext = ({ children }) => {
 const CountriesProvider = memo(SetContext);
 
 export default CountriesProvider;
-
-
-
-
-/* 
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import SimpleUserTable from "./SimpleUserTable";
-
-const USER_SERVICE_URL = 'https://jsonplaceholder.typicode.com/users';
-
-
-function UserTableReactHooks() {
-    const [data, setData] = useState({users: [], isFetching: false});
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                setData({users: data.users, isFetching: true});
-                const response = await axios.get(USER_SERVICE_URL);
-                setData({users: response.data, isFetching: false});
-            } catch (e) {
-                console.log(e);
-                setData({users: data.users, isFetching: false});
-            }
-        };
-        fetchUsers();
-    }, []);
-
-    return <SimpleUserTable data={data.users}
-                            isFetching={data.isFetching}
-    />
-}
-
-export default UserTableReactHooks
- */
