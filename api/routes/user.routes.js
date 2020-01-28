7ab3acbfb15d7b2cdfa9d952d8d3e5ebab7d7a19
Geoffrey Siele <sielekrisp@gmail.com>
2020-01-28 21:54:45 +0300

    Improve comment clarity api/routes/user.routes.js

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,31 +1,31 @@
 const router   = require('express').Router();
 
-// For protecting routes that require log in
+// For protecting routes that require log-in
 const isLoggedIn = require('../middleware/check-auth');
 
 // UC --> UserControllers
 const UC = require('../controllers/user.controllers');
 
-// GET --> Fetch all users
+// GET /users --> Fetch all users
 router.get('/', UC.fetchAllUsers);
 
-// GET --> Fetch a single user
-router.get('/:userId', UC.fetchSingleUser);
+// GET /users/:username --> Fetch a single user
+router.get('/:username', UC.fetchSingleUser);
 
-// PATCH --> Edit user
-router.patch('/:userId/edit', isLoggedIn, UC.editUser);
+// PATCH /users/:username/edit --> Edit user --> Login required
+router.patch('/:username/edit', isLoggedIn, UC.editUser);
 
-// DELETE --> Delete user
-router.delete('/:userId/del', isLoggedIn, UC.deleteUser);
+// DELETE /users/:username/del --> Delete user --> Login required
+router.delete('/:username/del', isLoggedIn, UC.deleteUser);
 
 module.exports = router;
 
 
 /*
 
-->GET    /users              --> Fetch all users
-->GET    /users/:userId      --> Fetch a single user
-->PATCH  /users/:userId/edit --> Edit user
-->DELETE /users/:userId/del  --> Delete user
+->GET    /users               --> Fetch all users
+->GET    /users/:userId       --> Fetch a single user
+->PATCH  /users/:userId/edit  --> Edit user
+->DELETE /users/:userId/del   --> Delete user
 
 */
