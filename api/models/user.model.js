4e0ec6e323a8ce53b4556b258ff6798f410e1491
Geoffrey Siele <sielekrisp@gmail.com>
2020-01-28 14:45:31 +0300

    Remove dead code from api/models/user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema(
   {
@@ -7,37 +6,11 @@ const UserSchema = mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
-    notes: []
   }, 
   {
     timestamps: true
   }
 );
-
-/* 
-UserSchema.pre('save', async function(next) {
-  try {
-    if (!this.isModified('password')) {
-      return next();
-    }
-
-    const hashed = await bcrypt.hash(this.password, 10);
-    this.password = hashed;
-    return next();
-
-  } catch(err) {
-    return next(err);
-  }
-});
-
-UserSchema.methods.comparePassword = async function(attempt, next) {
-  try {
-    return await bcrypt.compare(attempt, this.password);
-  } catch (err) {
-    next(err);
-  }
-}
-*/
 
 const User = mongoose.model('User', UserSchema);
 
