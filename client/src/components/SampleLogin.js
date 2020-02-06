1828c84e6e83194b410fa6a4c7ea856e788d0c22
Geoffrey Siele <sielekrisp@gmail.com>
2020-02-06 09:18:47 +0300

    Add mongodb as dev dependency

--- client/src/components/SampleLogin.js
+++ client/src/components/SampleLogin.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SampleLogin() {
   return (
-    <div>
+    <div style={{ margin: "20px" }}>
       <h2>Test Account Credentials</h2>
       <p>email: johndoe@email.com</p>
       <p>password: passpass</p><br/>
