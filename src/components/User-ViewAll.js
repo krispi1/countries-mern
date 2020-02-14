dd3aa23f195955864f3e1f286895ca7485977bc0
Geoffrey Siele <sielekrisp@gmail.com>
2020-02-14 20:08:46 +0300

    Fetch all users from DB and lay them out on page. No styling yet

--- src/components/User-ViewAll.js
+++ src/components/User-ViewAll.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import axios from 'axios';
 
 function Users() {
+
+  const [users, setUsers] = useState([]);
+
+  const fetchAllUsers = async () => {
+    const result = await(axios.get('/api/users'));
+    setUsers(result.data.users);
+    // console.log(result.data.users);
+  } // fetchAllUsers
+
+  useMemo(fetchAllUsers, []);
+
   return (
     <div>
-            
+      {
+        users && users.map((user, index) => {
+          return <div key={index}>{ user.username }</div>
+        })
+      }
     </div>
-  )
-}
+  ) // return
+} // Users
 
 export default Users;
