04c2cd0e04756bce926fc81e3ca42db5cce2d0b3
Geoffrey Siele <sielekrisp@gmail.com>
2020-02-28 13:45:56 +0300

    Restructure navbar -- src/components/Navbar.js -- to favor small devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,92 @@
 // Modules
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 // Context
-import { UserContext } from '../contexts/UserContext';
+import { UserContext } from "../contexts/UserContext";
 
 // Service
-import AuthService from '../services/auth.service';
+import AuthService from "../services/auth.service";
 
 function Navbar({ history }) {
   const { isLoggedIn } = useContext(UserContext);
 
-  return (
-    <nav>
-      {
-        isLoggedIn ? (
-          <div className="navbar-links">
-            <div><Link className="links-in-navbar" to="/">Home</Link></div>
-            <div><Link className="links-in-navbar" to="" onClick={ AuthService.logout }>Logout</Link></div>
-          </div>
-        ) : (
-          <div className="navbar-links">
-            <div><Link className="links-in-navbar" to="/">Home</Link></div>
-            <div><Link className="links-in-navbar" to="/samplelogin">TestAccount</Link></div>
-            <div><Link className="links-in-navbar" to="/signin">SignIn</Link></div>
-            <div><Link className="links-in-navbar" to="/signup">SignUp</Link></div>
-          </div>
-        )
-      }
-      
-    </nav>
-  )
-}
+  function toggleMenu() {
+    const navbarDiv = document.getElementById("navbarDiv");
+    
+    navbarDiv.classList.contains('invisible') ?
+    navbarDiv.classList.remove('invisible') :
+    navbarDiv.classList.add('invisible')
+  }; // setupNav
+
+  // Small screen/device
+  while (window.visualViewport.width <= 630) {
+    //  navbarDiv.ClassList.add();
+    return (
+      <>
+        <div 
+          id="navHeader"
+          className="nav-header"
+          onClick={ toggleMenu }>
+          -- MENU --
+        </div>
+        
+        <nav id="navbarDiv" className="invisible">
+        {
+          (!isLoggedIn) ?
+          (
+            <div className="navbar-links">
+              <div><Link className="links-in-navbar" to="/">Home</Link></div>
+              <div><Link className="links-in-navbar" to="/samplelogin">TestAccount</Link></div>
+              <div><Link className="links-in-navbar" to="/goodies">Goodies</Link></div>
+              <div><Link className="links-in-navbar" to="/signin">SignIn</Link></div>
+              <div><Link className="links-in-navbar" to="/signup">SignUp</Link></div>
+            </div>
+          ) : 
+          (
+            <div className="navbar-links">
+              <div><Link className="links-in-navbar" to="/">Home</Link></div>
+              <div><Link className="links-in-navbar" to="/samplelogin">TestAccount</Link></div>
+              <div><Link className="links-in-navbar" to="/goodies">Goodies</Link></div>
+              <div><Link className="links-in-navbar" to="" onClick={ AuthService.logout }>Logout</Link></div>
+            </div>
+          )
+        }
+        </nav>
+      </>
+    );
+  }
+
+  // Big screen/device
+  while (window.visualViewport.width >= 630.01) {
+  
+    return (
+      <>
+        <nav id="navbarDiv">
+        {
+          (!isLoggedIn) ?
+          (
+            <div className="navbar-links">
+              <div><Link className="links-in-navbar" to="/">Home</Link></div>
+              <div><Link className="links-in-navbar" to="/samplelogin">TestAccount</Link></div>
+              <div><Link className="links-in-navbar" to="/goodies">Goodies</Link></div>
+              <div><Link className="links-in-navbar" to="/signin">SignIn</Link></div>
+              <div><Link className="links-in-navbar" to="/signup">SignUp</Link></div>
+            </div>
+          ) : 
+          (
+            <div className="navbar-links">
+              <div><Link className="links-in-navbar" to="/">Home</Link></div>
+              <div><Link className="links-in-navbar" to="/samplelogin">TestAccount</Link></div>
+              <div><Link className="links-in-navbar" to="/goodies">Goodies</Link></div>
+              <div><Link className="links-in-navbar" to="" onClick={ AuthService.logout }>Logout</Link></div>
+            </div>
+          )
+        }
+        </nav>
+      </>
+    );
+  }
+} // Navbar
 
 export default Navbar;
-
